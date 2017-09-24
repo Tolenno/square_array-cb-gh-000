0cbd0e68f909f3b7d3d0c428d869bf5ebf4872ba
Matt Hough <matt@hough.tv>
2017-09-24 22:26:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,6 @@
-array = [1,2,3,4,5,6]
-
 def square_array(array)
   array.each do |number|
     sq_array = []
     sq_array << number**2
   end
-  return sq_array
-  puts sq_array
 end
-
-square_array(array)
