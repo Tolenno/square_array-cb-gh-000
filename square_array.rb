7e41d8484be4fb00585893080589aa20f27906fd
Matt Hough <matt@hough.tv>
2017-09-24 22:24:54 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 array = [1,2,3,4,5,6]
 
 def square_array(array)
@@ -8,7 +6,6 @@ def square_array(array)
     square_array << number**2
     return square_array
   end
-  binding.pry
 end
 
 square_array(array)
